1e08b2de0ea9671306d2a8717fb551ee3bfb50c2
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-03-22 21:12:20 -0400

    added sendgrid

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const functions = require("@google-cloud/functions-framework");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 // Register a CloudEvent callback with the Functions Framework that will
 // be executed when the Pub/Sub trigger topic receives a message.
-functions.cloudEvent("verify_email", (cloudEvent) => {
+functions.cloudEvent("verify_email", async (cloudEvent) => {
   // The Pub/Sub message is passed as the CloudEvent's data payload.
-  const base64name = cloudEvent.data.message.data;
+  const email = cloudEvent.data.username;
 
-  const name = base64name
-    ? Buffer.from(base64name, "base64").toString()
-    : "World";
-
-  console.log(`Hello, ${name}!`);
+  const msg = {
+    to: email,
+    from: "thakkar.kara@northeastern.edu",
+    subject: "Email Verification CSYE6225",
+    text: "Please verify your email using the link below",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  try {
+    await sgMail.send(msg);
+    console.log(`Email sent to ${name}`);
+  } catch (error) {
+    console.error(error);
+  }
 });
